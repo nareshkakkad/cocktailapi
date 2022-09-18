5193690eef883885a7810a7b9a9a7ee3ab04347a
Naresh Kakkad <naresh.kakkad@gmail.com>
2022-09-18 22:30:39 +0530

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+       Hi, i am Naresh Kakkad. Thanks for visiting our website.
+       any Query please contact this number: 9033063099 
+
+      </p>
+    </section>
+  );
+}
